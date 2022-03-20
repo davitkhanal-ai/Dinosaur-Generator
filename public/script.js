fc3fe9b151d60348220cc0f7c00276e1787715ec
davitkhanal <khanaldavit@gmail.com>
2022-03-20 20:06:48 +0545

    push code

--- public/script.js
+++ public/script.js
@@ -1,25 +1,40 @@
+console.log('script.js loaded');
 
-document.querySelector('#btnload').addEventListener('click', ()=>{
-    getDinoName();
-    getDinoImage();
-})
+document.querySelector('#btnLoad').addEventListener('click', () => {
+	if (document.querySelector('#dinoName') !== null) {
+		document.querySelector('#dinoName').remove();
+	}
+	if (document.querySelector('#dinoImage') !== null) {
+		document.querySelector('#dinoImage').remove();
+	}
+	getDinoName();
+});
 
+async function getDinoName() {
+	const response = await fetch('/dinoname');
+	const data = await response.json();
+	let dinoName = data[0].join(' ');
+	console.log(dinoName);
 
+	let dinoNameDiv = document.createElement('div');
+	dinoNameDiv.id = 'dinoName';
+	dinoNameDiv.textContent = dinoName;
+	document.querySelector('#dinoWrapper').appendChild(dinoNameDiv);
 
-getDinoName()
-getDinoImage()
-
-async function getDinoName(){
-    const response = await fetch("/dinoname");
-    const data = await response.json();
-    let dinoName = data[0].join(' ')
-    console.log(dinoName);
+	getDinoImage();
 }
 
-async function getDinoImage(){
-    const response = await fetch("/dinoimage");
-    const data = await response.json();
-    let dinoImage = data.value[Math.floor(Math.random() * data.value.length)].thumbnailUrl;
-    
-    console.log(dinoImage);
+async function getDinoImage() {
+	const response = await fetch('/dinoimage');
+	const data = await response.json();
+	let dinoImage = data.value[Math.floor(Math.random() * data.value.length)];
+	let dinoImageUrl = dinoImage.thumbnailUrl;
+	let dinoAlt = dinoImage.name;
+	console.log(dinoImage);
+
+	let img = document.createElement('img');
+	img.id = 'dinoImage';
+	img.src = dinoImageUrl;
+	img.alt = dinoAlt;
+	document.querySelector('#dinoWrapper').appendChild(img);
 }
