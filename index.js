984d309f3a166be937b92ec54de0f6d2f71d3b0b
davitkhanal <khanaldavit@gmail.com>
2022-03-20 20:01:35 +0545

    generating image

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.listen(port, ()=>{
 })
 
 
-//routes
+//routes for dino name
 app.get("/dinoname", async(req,response) =>{
     //coding stuff
     const fetchApi = await fetch(
@@ -29,4 +29,20 @@ app.get("/dinoname", async(req,response) =>{
     const dinoNameResponse = await fetchApi.json();
     console.log(dinoNameResponse)
     response.json(dinoNameResponse)
+})
+
+
+//routes for dino image
+app.get("/dinoimage", async(req,response) =>{
+    //coding stuff
+    const fetchApi = await fetch("https://bing-image-search1.p.rapidapi.com/images/search?q=dinosaur&count=20", {
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-host": "bing-image-search1.p.rapidapi.com",
+            "x-rapidapi-key": "68b0a54ebcmsh3f51a4d9b5fb940p12bf0fjsnbb5b3a3de47d"
+        }
+    })
+    const dinoImageResponse = await fetchApi.json();
+    console.log(dinoImageResponse)
+    response.json(dinoImageResponse)
 })
--- public/script.js
+++ public/script.js
@@ -1,9 +1,25 @@
 
+document.querySelector('#btnload').addEventListener('click', ()=>{
+    getDinoName();
+    getDinoImage();
+})
+
+
 
 getDinoName()
+getDinoImage()
+
 async function getDinoName(){
     const response = await fetch("/dinoname");
     const data = await response.json();
     let dinoName = data[0].join(' ')
     console.log(dinoName);
+}
+
+async function getDinoImage(){
+    const response = await fetch("/dinoimage");
+    const data = await response.json();
+    let dinoImage = data.value[Math.floor(Math.random() * data.value.length)].thumbnailUrl;
+    
+    console.log(dinoImage);
 }
